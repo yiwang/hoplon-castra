63f9446d495b9126792b6dc82bf0524a2b3f57c2
Yi Wang <e@yi-wang.me>
2015-02-15 23:55:33 -0500

    Castra 3: Modifying a Castra endpoint

--- src/clj/hoplon_castra/api.clj
+++ src/clj/hoplon_castra/api.clj
@@ -1,5 +1,9 @@
 (ns hoplon-castra.api
   (:require [tailrecursion.castra :refer [defrpc]]))
 
+(def counter (atom 0))
+
 (defrpc get-state []
-  {:random (rand-int 100)})
+  {:rpc/query [{:random (rand-int 100)
+                :counter @counter}]}
+  (swap! counter inc))
